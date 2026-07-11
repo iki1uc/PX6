0f4e892296107356ac43e32e3ed06f9861a69ae8
iki1uc <entertrainer@web.de>
2026-07-12 00:03:36 +0200

    Create minimal.js

---
+++ minimal.js
@@ -0,0 +1,11 @@
+// PX3 – 3er-Bildung
+function PX3(x){ return x + 3 }
+
+// PX4 – 4er-Bildung
+function PX4(x){ return x * 4 }
+
+// PX6 – 6er-Bildung
+function PX6(x){ return x ** 6 }
+
+// PX9 – 9er-Orbit
+function PX9(x){ return Math.sin(x) * 9 }
